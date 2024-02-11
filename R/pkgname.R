0bb2db717e723ef5dde78b01293d442e548ede8b
David Earn <earn@math.mcmaster.ca>
2024-02-11 16:26:53 -0500

    R CMD check produces no errors, but still some warnings

--- R/pkgname.R
+++ R/pkgname.R
@@ -8,8 +8,9 @@
 #' populations.
 #'
 #' This is the version of the `evolvr` package used to make the
-#' figures in Molina and Earn (2023).  The latest version of the
-#' package is available on github at
+#' figures in Molina and Earn (2024, Proc. R. Soc. A, 20230305,
+#' \url{https://doi.org/10.1098/rspa.2023.0305}).  The
+#' latest version of the package is available on github at
 #' \url{https://github.com/davidearn/evolvr} and might also be
 #' available on CRAN.
 #'
